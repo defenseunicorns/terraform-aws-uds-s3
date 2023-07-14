3edc5edee6f3bba569aca4c4ce68dd15590cd572
Lucas Rodriguez <lucas.rodriguez@defenseunicorns.com>
2023-07-14 15:13:51 -0500

    Add bucket ARN output

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,9 @@
-output "s3_bucket" {
+output "bucket_name" {
   description = "S3 Bucket Name"
   value       = module.s3_bucket.s3_bucket_id
+}
+
+output "bucket_arn" {
+  description = "S3 Bucket ARN"
+  value       = module.s3_bucket.s3_bucket_arn
 }
